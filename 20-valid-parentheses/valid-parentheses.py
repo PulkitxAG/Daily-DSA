d70a5fc3498cac99b7405ae3690c047dbf6ade4f
Pulkit Agrawal <pulkitagrawal089@gmail.com>
2026-09-07 23:25:31 +0530

    Time: 3 ms (32.18%) | Memory: 19.2 MB (91.11%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {")":"(",
+                "]":"[",
+                "}":"{"
+            }
+        stack = []
+
+        for ch in s:
+            if ch in "({[":
+                stack.append(ch)
+            else:
+                if not stack:
+                    return False
+                top = stack.pop()
+                if top != dic[ch]:
+                    return False
+        return len(stack) == 0
