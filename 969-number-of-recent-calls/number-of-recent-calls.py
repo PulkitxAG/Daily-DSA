dbd8aaff6bdfa9c61bbea833d99875f24a3c2622
Pulkit Agrawal <pulkitagrawal089@gmail.com>
2026-09-15 18:59:29 +0530

    Time: 21 ms (95.75%) | Memory: 24.2 MB (83.63%) - LeetSync

---
+++ 969-number-of-recent-calls/number-of-recent-calls.py
@@ -0,0 +1,18 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.ans = []
+
+    def ping(self, t: int) -> int:
+        self.ans.append(t)
+
+        while self.ans[0] < t - 3000:
+            self.ans.pop(0)
+        
+        return len(self.ans)
+        
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
