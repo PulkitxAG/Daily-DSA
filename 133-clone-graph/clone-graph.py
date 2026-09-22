0e41dbfeb94d204ef3d7473280d454cdb88f58da
Pulkit Agrawal <pulkitagrawal089@gmail.com>
2026-09-22 22:09:38 +0530

    Time: 49 ms (59.87%) | Memory: 19.7 MB (88.11%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -1,32 +1,18 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-"""
-
-from collections import deque
-from typing import Optional
-
-class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if node is None:
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        if not node:
             return None
-        clone = {}
-        visited = set()
-        q = deque()
-        q.append(node)
-        clone[node] = Node(node.val)
-        while q:
-            curr = q.popleft()
-            if curr in visited:
-                continue
-            visited.add(curr)
-            for nei in curr.neighbors:
-                if nei not in clone:
-                    clone[nei] = Node(nei.val)
-                clone[curr].neighbors.append(clone[nei])
-                if nei not in visited:
-                    q.append(nei)
-        return clone[node]
+        visited = {}
+        def clone(node):
+            if node in visited:
+                return visited[node]
+            new_node = Node(node.val)
+            visited[node] = new_node
+            for neighbros in node.neighbors:
+                new_node.neighbors.append(clone(neighbros))
+            return new_node
+        return clone(node)
